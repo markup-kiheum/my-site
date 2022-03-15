8cc4941cf3d793cc53543cf4fa4cdf418e495dc8
Kiheum <markup.kiheum@gmail.com>
2022-03-15 16:40:17 +0900

    save flie

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPlugins([
+  nextConfig,
+  [optimizedImages, {
+    images: {
+      loader: 'akamai',
+      path: '/',
+    }
+  }],
+
+  // your other plugins here
+
+]);
